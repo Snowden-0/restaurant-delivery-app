698f962a5911780cbde237624b84ba74c724d328
Muhammad Ubaid ur Rehman <uurrehman735@gmail.com>
2025-07-11 15:39:37 +0500

    Removed import of down method

--- migrations/20250711095603-uuid-ossp.cjs
+++ migrations/20250711095603-uuid-ossp.cjs
@@ -1,7 +1,5 @@
 'use strict'
 
-const { down } = require("./20250710124042-create-user.cjs");
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // Run a raw SQL query to install the "uuid-ossp" extension
