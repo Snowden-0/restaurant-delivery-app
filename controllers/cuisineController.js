4403fe48276b8f1f4190ac813a89da25ce8bccec
Muhammad Ubaid ur Rehman <uurrehman735@gmail.com>
2025-07-18 12:46:12 +0500

    resolved issues in cuisine controller

--- controllers/cuisineController.js
+++ controllers/cuisineController.js
@@ -6,26 +6,24 @@ const SERVER_ERROR = 'Internal server error.';
 export const getAllCuisines = async (req, res) => {
   try {
     const cuisines = await cuisineService.getAllCuisines();
-    res.status(200).json(cuisines);
+    return res.status(200).json(cuisines);
   } catch (error) {
-    //console.error('Error in getAllCuisines controller:', error);
-    res.status(500).json({ message: {SERVER_ERROR} });
+    return res.status(500).json({ message: SERVER_ERROR });
   }
 };
 
 
 export const getRestaurantsByCuisine = async (req, res) => {
   try {
-    const { cuisineId } = req.params; // Get cuisineId from URL parameters
+    const { cuisineId } = req.params; 
     const restaurants = await cuisineService.getRestaurantsByCuisineId(cuisineId);
 
     if (restaurants.length === 0) {
-      return res.status(404).json({ message: {NO_RESTAURANTS_ERROR} });
+      return res.status(404).json({ message: NO_RESTAURANTS_ERROR });
     }
 
-    res.status(200).json(restaurants);
+    return res.status(200).json(restaurants);
   } catch (error) {
-    //console.error('Error in getRestaurantsByCuisine controller:', error);
-    res.status(500).json({ message: {SERVER_ERROR} });
+    return res.status(500).json({ message: SERVER_ERROR});
   }
 };
